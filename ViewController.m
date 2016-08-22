1da0ef148c2a9911582fa1e428ee6185607731e7
YihengYao <fn1117@qq.com>
2016-08-23 01:08:44 +1200

    去掉注释

--- ViewController.m
+++ ViewController.m
@@ -10,7 +10,7 @@
 
 @interface ViewController ()
 {
-    //保存图片的个数
+
     NSDictionary *_dict;
 }
 
@@ -22,67 +22,51 @@
     [super viewDidLoad];
     NSBundle *bundle = [NSBundle mainBundle];
     NSString *path = [bundle pathForResource:@"Property List" ofType:@"plist"];
-    //通过NSBundle下的路径创建字典
     _dict = [NSDictionary dictionaryWithContentsOfFile:path];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 #pragma mark 传入动画的名称以及图片数
 -(void)animationWithFuncName:(NSString *)name picNo:(int)num{
 
-    //如果动画正在播放则返回, 执行效果为, 在没有结束上一个动画前, 新动画不执行
+
     if (_tom.isAnimating) {
         return;
     }
-    //创建一个可变数组用来添加UIImage对象
+
     NSMutableArray *images = [NSMutableArray array];
     for (int i = 0; i < num; i++) {
-        //图片名
+
         NSString *imgName = [NSString stringWithFormat:@"%@_%02d.jpg", name, i];
 
-        //通过NSBundle加载图片全路径, 而且可以直接写成 ofType:nil
+
         NSString *path = [[NSBundle mainBundle] pathForResource:imgName ofType:nil];
-        //使用该方法没有缓存问题.
+
         UIImage *img = [[UIImage alloc] initWithContentsOfFile:path];
 
-        //如果项目中经常需要用到该图片,则选择imageNamed:的方法,
-        //如果图片加载是一次性的,或者图片比较大的 选择上面的方法
-        //UIImage *img = [UIImage imageNamed:@""];
         [images addObject:img];
     }
-    //设置动画的图片
+
     self.tom.animationImages = images;
 
-    //设置动画的持续时间
     self.tom.animationDuration = 0.1 * num;
 
-    //设置动画的重复次数
     self.tom.animationRepeatCount = 1;
-    //设置动画开始
     [self.tom startAnimating];
-    //以下写法会报错
-    //Property access result unused - getters should not be used for side effects
-    //_tom.startAnimating;
+
 
 }
 
-/*每次点击该方法都需要进入NSBundle中进行读写字典的操作,
- 但其实只可以做一次操作, 既然只需要做一次,
- 就可以将其设置为成员变量, 并在viewDidLoad方法中加载.
- */
 #pragma mark 通过UIButton获取用户点击的按钮, 并通过字典输出相应的动作名称与所对应的值
 - (IBAction)function:(UIButton *)sender {
 
 
 
-    //通过该方法访问按钮UIControlStateNormal状态下的文字
+
     NSString *functionName = [sender titleForState:UIControlStateNormal];
     int picNo = [_dict[functionName] intValue];
-    //调用方法, 传入动画名与图片张数.
+
     [self animationWithFuncName:functionName picNo:picNo];
 
 
